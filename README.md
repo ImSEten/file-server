18f061c3f6be4e267f93ec55a38fc222fd45c540
ImSEten <zhangyu_ok@163.com>
2024-12-02 20:27:55 +0800

    add requirement to test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # file-server
 file-server is a file server writen by rust
+
+# build requirement
+libssl-dev
+protobuf-compiler
--- scripts/install_protoc.sh
+++ scripts/install_protoc.sh
@@ -7,12 +7,15 @@ if [ "$RUNNER_OS" == 'Linux' ]; then
     # Install on Linux
     sudo apt-get update
     sudo apt-get install -y protobuf-compiler
+    sudo apt-get install libssl-dev
 elif [ "$RUNNER_OS" == 'macOS' ]; then
     # Install on macOS
     brew install protobuf
+    brew install libssl-dev
 elif [ "$RUNNER_OS" == 'Windows' ]; then
     # Install on Windows
     choco install -y protoc
+    choco install -u libssl-dev
 else
     echo "Unsupported OS: $RUNNER_OS"
     exit 1
